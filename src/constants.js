28c35870b3fc8d221bb218d5473c190acf1ba5cf
AndreQ091 <vea86vvkoeyl@mail.ru>
2021-01-08 13:23:44 +0300

    finish redux and table logic

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const defaultStyles = {
+  textAlign: 'left',
+  fontWeight: 'normal',
+  textDecoration: 'none',
+  fontSize: 'normal'
+}
+
+export const defaultTitle = 'Новая таблица'
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,8 @@
 class Dom {
   constructor(selector) {
-    this.$el = typeof selector === 'string' ?
-            document.querySelector(selector) :
-            selector
+    this.$el = typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector
   }
 
   html(html) {
@@ -12,8 +12,9 @@ class Dom {
     }
     return this.$el.outerHTML.trim()
   }
+
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text
       return this
     }
@@ -27,43 +28,71 @@ class Dom {
     this.html('')
     return this
   }
+
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
+
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
   }
+
   find(selector) {
     return $(this.$el.querySelector(selector))
   }
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value)
+      return this
+    }
+    return this.$el.getAttribute(name)
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
     }
+
     if (Element.prototype.append) {
       this.$el.append(node)
     } else {
       this.$el.appendChild(node)
     }
+
     return this
   }
+
   get data() {
     return this.$el.dataset
   }
+
   closest(selector) {
     return $(this.$el.closest(selector))
   }
+
   getCoords() {
     return this.$el.getBoundingClientRect()
   }
+
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
+
   css(styles = {}) {
-    Object.keys(styles).forEach(key => {
-      this.$el.style[key] = styles[key]
-    })
+    Object
+        .keys(styles)
+        .forEach(key => {
+          this.$el.style[key] = styles[key]
+        })
   }
+
+  getStyles(styles = []) {
+    return styles.reduce((res, s) => {
+      res[s] = this.$el.style[s]
+      return res
+    }, {})
+  }
+
   id(parse) {
     if (parse) {
       const parsed = this.id().split(':')
@@ -74,14 +103,17 @@ class Dom {
     }
     return this.data.id
   }
+
   focus() {
     this.$el.focus()
     return this
   }
+
   addClass(className) {
     this.$el.classList.add(className)
     return this
   }
+
   removeClass(className) {
     this.$el.classList.remove(className)
     return this
